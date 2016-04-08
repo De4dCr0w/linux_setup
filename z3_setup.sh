802ab5ac9484a69b58ac951b6c48400a3ccad75b
meh <meh@ubuntu-linux-15.localdomain>
2016-04-08 20:55:06 +0800

    update z3_setup.sh

--- z3_setup.sh
+++ z3_setup.sh
@@ -2,7 +2,7 @@
 cd $HOME
 git clone https://github.com/Z3Prover/z3
 cd $HOME/z3
-python scripts/mk_make.py --prefix=$HOME
+python scripts/mk_make.py --prefix=/usr --python --pypkgdir=/usr/lib/python-2.7
 cd build
 make
 sudo make install
