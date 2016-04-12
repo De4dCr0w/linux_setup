90e22beb2cd35b1c24e39c9e0a78b02472b41f23
meh <meh@ubuntu-linux-15.localdomain>
2016-04-12 21:01:41 +0800

    update libc_setup.sh

--- libc_setup.sh
+++ libc_setup.sh
@@ -3,17 +3,19 @@ mkdir $HOME/src
 cd $HOME/src
 wget "http://ftp.gnu.org/gnu/glibc/glibc-$1.tar.gz"
 tar xvf "glibc-$1.tar.gz"
+rm -rf "glibc-$1.tar.gz"
 mkdir -p "$HOME/build/glibc-$1"
 cd "$HOME/build/glibc-$1"
-"$HOME/src/glibc-$1/configure" --prefix=/usr
+"$HOME/src/glibc-$1/configure" --prefix=/usr \
+    CFLAGS="-g -g3 -ggdb -gdwarf-4 -Og" \
+    CXXFLAGS="-g -g3 -ggdb -gdwarf-4 -Og"
 make
 # 32
 mkdir "$HOME/build/glibc32-$1"
 cd "$HOME/build/glibc32-$1"
-"$HOME/src/glibc-$1/configure" --prefix="$HOME/glibc32-$1" \
+"$HOME/src/glibc-$1/configure" --prefix=/usr \
     --host=i686-linux-gnu \
-    --build=i686-linux-gnu \
-    CC="gcc -m32 -g" CXX="g++ -m32 -g" \
-    CFLAGS="-O2 -march=i686" \
-    CXXFLAGS="-O2 -march=i686"
+    CC="gcc -m32" CXX="g++ -m32" \
+    CFLAGS="-g -g3 -ggdb -gdwarf-4 -Og" \
+    CXXFLAGS="-g -g3 -ggdb -gdwarf-4 -Og"
 make
