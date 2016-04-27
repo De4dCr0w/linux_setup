8c0b5d1f6db737b6376e0725e61ff2d81ffa28ba
meh <meh@ubuntu-16.04>
2016-04-27 15:25:24 +0800

    update libc_setup

--- libc_setup.sh
+++ libc_setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-mkdir $HOME/src
+mkdir -p $HOME/src
 cd $HOME/src
+# 64
 wget "http://ftp.gnu.org/gnu/glibc/glibc-$1.tar.gz"
 tar xvf "glibc-$1.tar.gz"
-rm -rf "glibc-$1.tar.gz"
 mkdir -p "$HOME/build/glibc-$1"
 cd "$HOME/build/glibc-$1"
-"$HOME/src/glibc-$1/configure" --prefix=/usr \
-    CFLAGS="-g -g3 -ggdb -gdwarf-4 -Og" \
-    CXXFLAGS="-g -g3 -ggdb -gdwarf-4 -Og"
+CFLAGS="-g -g3 -ggdb -gdwarf-4 -Og -Wno-error=maybe-uninitialized" \
+    CXXFLAGS="-g -g3 -ggdb -gdwarf-4 -Og -Wno-error=maybe-uninitialized" \
+    "$HOME/src/glibc-$1/configure" --prefix=/usr 
 make
 # 32
-mkdir "$HOME/build/glibc32-$1"
+mkdir -p "$HOME/build/glibc32-$1"
 cd "$HOME/build/glibc32-$1"
-"$HOME/src/glibc-$1/configure" --prefix=/usr \
-    --host=i686-linux-gnu \
-    CC="gcc -m32" CXX="g++ -m32" \
-    CFLAGS="-g -g3 -ggdb -gdwarf-4 -Og" \
-    CXXFLAGS="-g -g3 -ggdb -gdwarf-4 -Og"
+CC="gcc -m32" CXX="g++ -m32" \
+    CFLAGS="-g -g3 -ggdb -gdwarf-4 -Og -Wno-error=maybe-uninitialized" \
+    CXXFLAGS="-g -g3 -ggdb -gdwarf-4 -Og -Wno-error=maybe-uninitialized" \
+    "$HOME/src/glibc-$1/configure" --prefix=/usr \
+    --host=i686-linux-gnu 
 make
